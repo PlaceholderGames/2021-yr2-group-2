6c1951348ffa6c504e118f891f597edb8667326e
Troy Knox <18064922@students.southwales.ac.uk>
2020-11-01 13:47:58 +0000

    Enemy movement added

---
+++ Assets/NewEnemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NewEnemyMovement : MonoBehaviour
+{
+    public float LookRadius = 10f;
+
+    Transform target;
+
+    NavMeshAgent agent;
+
+     public GameObject player;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.LookAt(player.transform);
+        transform.position += transform.forward * 5f * Time.deltaTime;
+
+    }
+
+    void OnDrawGizmosSelected ()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, LookRadius);
+    }
+}
