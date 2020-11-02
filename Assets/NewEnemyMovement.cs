f865951be580b02b762a33976e303d9d7cfec3dd
Troy Knox <18064922@students.southwales.ac.uk>
2020-11-02 13:32:54 +0000

    Enemy movemen Fixed

--- Assets/NewEnemyMovement.cs
+++ Assets/NewEnemyMovement.cs
@@ -9,25 +9,40 @@ public class NewEnemyMovement : MonoBehaviour
 
     Transform target;
 
+    public bool found = false;
+
     NavMeshAgent agent;
 
-     public GameObject player;
+    public GameObject player;
 
     void Start()
     {
-        
+        //allows ai to find player and recognise where the player is 
+        GameObject obj = GameObject.Find("Player");
+        target = obj.transform;
+        //error showing if enemy cannot find player
+        if(obj = null)
+        {
+            print("Error enemy finding player");
+        }
     }
-
+    //movement of the character
     void Update()
     {
-        transform.LookAt(player.transform);
-        transform.position += transform.forward * 5f * Time.deltaTime;
-
+        if (found == true)
+        {
+            transform.LookAt(target.transform);
+            transform.position += transform.forward * 5f * Time.deltaTime;
+        }
     }
-
-    void OnDrawGizmosSelected ()
+//allowing trigger to recognise player
+    private void OnTriggerEnter(Collider other)
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, LookRadius);
+        if(other.name == "Player")
+        {
+            found = true;
+        }
     }
+    
+
 }
