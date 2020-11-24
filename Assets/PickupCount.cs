c81acb59f4d2d02f6b3db97b8ea1336302588f61
Isaac Raphael Jones <30005209@students.southwales.ac.uk>
2020-11-24 21:36:25 +0000

    added pickup power with canvas UI

---
+++ Assets/PickupCount.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupCount : MonoBehaviour
+{
+    //Progress bar for pickup
+    ProgressBar pickup;
+
+    //Completion colour
+    [Tooltip("The colour the bar will go when complete")]
+    public Color CompletionColour;
+
+    private void Start()
+    {
+        //Get the pickup progress bar 
+        pickup = GetComponentInParent<ProgressBar>();
+
+        //Set the maximum to be the total number of collectibles in the level
+        pickup.maximum = GameObject.FindGameObjectsWithTag("Lvl Complete Pickup").Length;
+        
+        //Set current to 0
+        pickup.current = 0;
+
+        //Set minimum to 0
+        pickup.minimum = 0;
+    }
+
+    private void Update()
+    {
+        //Set the current count to the maximum - number remaining
+        pickup.current = pickup.maximum - GameObject.FindGameObjectsWithTag("Lvl Complete Pickup").Length;
+        
+        //When all collected change colour to clearly show all collected
+        if (pickup.current == pickup.maximum)
+        {
+            pickup.fill_color = CompletionColour;
+        }
+
+    }
+}
