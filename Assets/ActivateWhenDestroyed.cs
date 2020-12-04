fb8c8a87c8aace8b5e56829ce77560f6a6698782
Isaac Raphael Jones <30005209@students.southwales.ac.uk>
2020-12-04 01:36:56 +0000

    Main menu made

--- Assets/ActivateWhenDestroyed.cs
+++ Assets/ActivateWhenDestroyed.cs
@@ -14,16 +14,17 @@ public class ActivateWhenDestroyed : MonoBehaviour
 
     private void OnDestroy()
     {
-        if (list.Length != 0)
-        {
-            Debug.Log(list.Length);
-            for (int i = 0; i < list.Length; i++)
-            {
-                if (list != null)
-                {
-                    list[i].SetActive(true);
-                }
-            }
-        }
+        //Destroy all other versions of this pickup
+        //if (list.Length != 0)
+        //{
+        //    Debug.Log(list.Length);
+        //    for (int i = 0; i < list.Length; i++)
+        //    {
+        //        if (list != null)
+        //        {
+        //            list[i].SetActive(true);
+        //        }
+        //    }
+        //}
     }
 }
---
+++ Assets/SetLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SetLevel : MonoBehaviour
+{
+    public int FirstLevel;
+    public void StartGame()
+    {
+        SceneManager.LoadScene(FirstLevel);
+    }
+
+    
+}
