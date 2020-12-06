71d3eeafe2c0f26e8a8952feaaa50297170ba439
alexboi98 <18085539@students.southwales.ac.uk>
2020-12-06 16:42:13 +0000

    Animations for the level

---
+++ Assets/AnimationScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent <Animation>()["BallAnimation"].time = Random.Range(0f , 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
