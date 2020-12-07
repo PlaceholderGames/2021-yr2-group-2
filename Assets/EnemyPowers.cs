9c6f239ba90659fdfee1b6e06ce8f4aef656c3e7
Isaac Raphael Jones <30005209@students.southwales.ac.uk>
2020-12-07 14:55:42 +0000

    Organising prefabs

---
+++ Assets/EnemyPowers.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPowers : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(GetComponentInParent<GhostPower>() != null)
+        {
+            Debug.Log("Has Ghost Power");
+            GetComponentInParent<GhostPower>().HandleGhostPower();
+        }
+        
+    }
+}
