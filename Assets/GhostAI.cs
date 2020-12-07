727711487609ac6db640919954cd18402c7ae400
Isaac Raphael Jones <30005209@students.southwales.ac.uk>
2020-12-07 15:25:46 +0000

    changed level structure

---
+++ Assets/GhostAI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+[ExecuteInEditMode()]
+public class GhostAI : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [MenuItem("GameObject/Enemies/Ghost")]
+    public static void AddGhost()
+    {
+        GameObject obj = Instantiate(Resources.Load<GameObject>("GameObject/Enemies/Ghost"));
+        obj.transform.SetParent(Selection.activeGameObject.transform, false);
+    }
+#endif
+
+
+
+}
