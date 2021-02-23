167fc6fa5618d3e48674528b5e3e3de6c2235f48
Nathan <nathanaelkinnis@hotmail.com>
2021-02-24 00:05:57 +0200

    speed run timer done and created a simple UI to show it on screen

---
+++ Assets/UI/SpeedRun.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedRun : MonoBehaviour
+{
+    Text _timeText;
+    //public Text _timerText;
+    // Update is called once per frame
+
+    void Start()
+    {
+        _timeText=gameObject.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        int minutes = Mathf.FloorToInt(Time.time / 60f);
+        int seconds = Mathf.FloorToInt(Time.time - minutes * 60);
+
+        string textTime= string.Format("{0:0}:{1:00}", minutes, seconds);
+
+        _timeText.text=textTime;
+
+    }
+}
