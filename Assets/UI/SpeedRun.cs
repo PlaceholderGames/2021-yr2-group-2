3a3eb573957c9492a865c86cd0e151b5d58b1255
Nathan <nathanaelkinnis@hotmail.com>
2021-03-03 01:08:03 +0200

    Trying to fix merge conflict

--- Assets/UI/SpeedRun.cs
+++ Assets/UI/SpeedRun.cs
@@ -7,23 +7,24 @@ using System;
 
 public class SpeedRun : MonoBehaviour
 {
-    //The text that will be affected from the update function
-    Text _timeText;
+    Text _timeText; //The text that will be affected from the update function
+    float startingTime = 0f; //Used to start the timer the moment the player starts the game from the main menu.
     static string textTime;
-
+    
     // Update is called once per frame
 
     void Start()
     {
         _timeText=gameObject.GetComponent<Text>(); //finds the text component
+        startingTime = Time.time;
     }
 
     void Update()
     {
         if (!(SceneManager.GetActiveScene().name == "WinScreen"))
         {
-            int minutes = Mathf.FloorToInt(Time.time / 60f);
-            string seconds = (Math.Round((Time.time - minutes * 60), 2)).ToString();
+            int minutes = Mathf.FloorToInt((Time.time - startingTime) / 60f);
+            string seconds = (Math.Round(((Time.time - startingTime) - minutes * 60), 2)).ToString();
             textTime = string.Format("{00:00}:{01:00}", minutes, seconds);
         }
         _timeText.text = textTime;
