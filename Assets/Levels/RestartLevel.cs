91ba93cf6d64cf7b80eb86cd05b0eb975181adaf
Nathan <nathanaelkinnis@hotmail.com>
2021-03-02 02:11:59 +0200

    Bug fixing

--- Assets/Levels/RestartLevel.cs
+++ Assets/Levels/RestartLevel.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class RestartLevel : MonoBehaviour
 {
+
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void OnDisable()
+    {
+        Time.timeScale = 1.0f;
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 }
