af5d10453ab71e19d2a68a5198c797541ab3538e
Isaac Raphael Jones <30005209@students.southwales.ac.uk>
2021-03-08 01:06:23 +0000

    Patrolpoint update

--- Assets/Enemy/PatrolPoint.cs
+++ Assets/Enemy/PatrolPoint.cs
@@ -11,48 +11,61 @@ public class PatrolPoint : MonoBehaviour
     [SerializeField]
     float CurTimer = 0.0f;
 
+    [Tooltip("Next Patrol Point in list")]
     [SerializeField]
     GameObject NextPatrolPoint = null;
 
     private void Start()
     {
+        //Set collider to the own collider
         OwnCollider = this.GetComponent<SphereCollider>();
+
+        //Set cur timer to be the preset timer amount
         CurTimer = ReactivateTimer;
 
+        //Check if there is no next patrol point
         if(NextPatrolPoint == null)
         {
             Debug.Log("No NextPatrolPoint set for " + this.gameObject.name);
         }
 
-
+        //Disable mesh to appear invisible
         this.GetComponent<MeshRenderer>().enabled = false;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        //If collided with enemy
         if (collision.gameObject.tag == "Enemy")
         {
+            //Deactivate the object (to avoid multiple collision triggers)
             OwnCollider.gameObject.SetActive(false);
+
+            //Reset timer
             CurTimer = ReactivateTimer;
         }
     }
 
     private void Update()
     {
+        //Whilst collider is deactivated count down cooldown
         if (!OwnCollider.gameObject.activeInHierarchy)
         {
             CurTimer -= Time.deltaTime;
         }
         else
         {
+            //Once timer is over
             if(CurTimer <= 0.1f)
             {
+                //Reactivate collider
                 OwnCollider.gameObject.SetActive(true);
             }
         }
 
     }
 
+    //Give the new control point
     public GameObject GiveNewPatrolPoint()
     {
         return NextPatrolPoint;
