6df736b4d951dcf446fc898cc9e8eff2cd87a1ab
30005209 <30005209@students.southwales.ac.uk>
2021-04-20 10:54:15 +0100

    added skyboxes to swamp levels

--- Assets/Levels/LvlCollectorPickup.cs
+++ Assets/Levels/LvlCollectorPickup.cs
@@ -9,11 +9,20 @@ public class LvlCollectorPickup : MonoBehaviour
     int curCount = 0;
     private void OnTriggerEnter(Collider other)
     {
+        GetComponent<AudioSource>().pitch = 1.0f;
         if (other.tag == "Lvl Complete Pickup")
         {
             curCount++;
             Destroy(other.gameObject);
+            GetComponent<AudioSource>().pitch = 1.25f;
             GetComponent<AudioSource>().Play();
         }
+        else if (other.tag == "PP Ghost")
+        {
+            Destroy(other.gameObject);
+            GetComponent<AudioSource>().Play();
+        }
+
+
     }
 }
